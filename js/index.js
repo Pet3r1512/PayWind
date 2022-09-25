8e302a8e490c055d3fa16cc3c2798bc9471d9277
Pet3r1512 <89619124+Pet3r1512@users.noreply.github.com>
2022-09-25 19:48:09 +0700

    Create Project

---
+++ js/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+const server = app.listen(3000, () => {
+    console.log(`The application started on port ${server.address().port}`);
+});
+
+const path = require('path');
+
+app.set('views', path.join(__dirname, '../views/user'));
+app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
