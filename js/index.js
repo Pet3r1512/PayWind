ef656dfb3f702e2e8f47c289073689219c70a3c7
Pet3r1512 <89619124+Pet3r1512@users.noreply.github.com>
2022-10-02 13:42:41 +0700

    Fix path to CSS file

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ const path = require('path');
 
 app.set('views', path.join(__dirname, '../views/user'));
 app.set('view engine', 'pug');
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, '../public')))
 
 app.get('/', (req, res) => {
     res.render('index');
