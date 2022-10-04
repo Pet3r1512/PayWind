92a92562f13b11d677454b5adf5f3470946383eb
lekhoi2709 <ledinhkhoimd@gmail.com>
2022-10-04 11:50:41 +0700

    build sign in modal

--- js/index.js
+++ js/index.js
@@ -3,15 +3,15 @@ const express = require('express');
 const app = express();
 
 const server = app.listen(3000, () => {
-    console.log(`The application started on port ${server.address().port}`);
+	console.log(`The application started on port ${server.address().port}`);
 });
 
 const path = require('path');
 
-app.set('views', path.join(__dirname, '../views/user'));
+app.set('views', path.join(__dirname, '../views/user/createAccount'));
 app.set('view engine', 'pug');
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.render('index');
+	res.render('index');
 });
