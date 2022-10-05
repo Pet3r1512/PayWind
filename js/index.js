47ba7aba082f39ace587e153ad3bf6117edab3bf
lekhoi2709 <ledinhkhoimd@gmail.com>
2022-10-05 07:09:42 +0700

    adjust tailwindcss

--- js/index.js
+++ js/index.js
@@ -2,16 +2,20 @@ const express = require('express');
 
 const app = express();
 
-const server = app.listen(3000, () => {
-    console.log(`The application started on port ${server.address().port}`);
-});
-
 const path = require('path');
 
-app.set('views', path.join(__dirname, '../views/user'));
+app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'pug');
-app.use(express.static(path.join(__dirname, '../public')))
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/', (req, res) => {
-    res.render('index');
+	res.render('user/index');
+});
+
+app.use((req, res) => {
+    res.status(404).render('errors');
+});
+
+const server = app.listen(3000, () => {
+	console.log(`The application started on port ${server.address().port}`);
 });
