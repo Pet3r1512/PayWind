e5de8a22ab08559ec94fa201073f203311f96171
lekhoi2709 <ledinhkhoimd@gmail.com>
2022-10-05 19:22:20 +0700

    get signin page

--- js/index.js
+++ js/index.js
@@ -8,10 +8,14 @@ const server = app.listen(3000, () => {
 
 const path = require('path');
 
-app.set('views', path.join(__dirname, '../views/user'));
+app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, '../public')))
 
+app.get('/signin', (req, res) => {
+    res.render('signIn/signIn')
+})
+
 app.get('/', (req, res) => {
-	res.render('index');
+	res.render('user/index');
 });
