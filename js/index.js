3d975ea2dc5d06d02ddf365567f2c4867c24c71b
Pet3r1512 <89619124+Pet3r1512@users.noreply.github.com>
2022-10-08 14:35:09 +0700

    {WIP} Vercel deployment

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const { v4 } = require('uuid');
 
 app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'pug');
@@ -21,6 +22,24 @@ app.use((err, req, res, next) => {
 	res.status(500).render('../views/errors/error_page', { error_title: '500 - Server Error', error_code: '500', error_header: "Uh oh! I think i broke it. Please report me to the system administrator!", error_des: "Internal Server Error" });
 })
 
+// const server = app.listen(3000, () => {
+// 	console.log(`The application started on port ${server.address().port}`);
+// });
+
+app.get('/js', (req, res) => {
+	const path = `/js/item/${v4()}`;
+	res.setHeader('Content-Type', 'text/html');
+	res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+	res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
+
+app.get('/js/item/:slug', (req, res) => {
+	const { slug } = req.params;
+	res.end(`Item: ${slug}`);
+});
+
 const server = app.listen(3000, () => {
 	console.log(`The application started on port ${server.address().port}`);
 });
+
+module.exports = app;
