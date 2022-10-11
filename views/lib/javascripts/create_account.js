b4bc6e537cc3467de0dc6c057750633b0f99da15
Pet3r1512 <89619124+Pet3r1512@users.noreply.github.com>
2022-10-11 20:43:04 +0700

    Check create account logic

---
+++ views/lib/javascripts/create_account.js
@@ -0,0 +1,6 @@
+function check_create_account() {
+    const password = document.getElementById('pass_input'),
+        confirmPassword = document.getElementById('confirm_pass')
+
+            ()
+}
