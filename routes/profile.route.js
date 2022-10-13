e4cf86c29f4ef901ad71f8d3df16b75ab3ccee1e
lekhoi2709 <ledinhkhoimd@gmail.com>
2022-10-13 18:59:27 +0700

    build profile page

--- routes/profile.route.js
+++ routes/profile.route.js
@@ -1,11 +1,33 @@
 const router = require('express').Router();
 
+var card = [
+    {
+        name: "1",
+        exp: "06/22",
+        id: "111111111"
+    },
+
+    {
+        name: "2",
+        exp: "08/22",
+        id: "222222222"
+    }
+]
+
 router.get('/change_password', (req, res) => {
     return res.render('../views/user/profile/change_password.pug')
 })
 
 router.get('/verify_account', (req, res) => {
     return res.render('../views/user/profile/verify_account.pug')
+})
+
+router.get('/:id', (req, res) => {
+    return res.render('../views/user/profile/profile.pug', {
+        id: req.params.id,
+        cards: card,
+        card_count: card.length
+    })
 })
 
 module.exports = router
