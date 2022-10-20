ed21b743c2fbea45688e646d62718844ea80d0cc
lekhoi2709 <ledinhkhoimd@gmail.com>
2022-10-20 11:47:48 +0700

    adjust showPass function

--- views/assets/javascripts/show_pass.js
+++ views/assets/javascripts/show_pass.js
@@ -14,23 +14,19 @@ function showPass(){
 
     if(pass.type == "password"){
         pass.type = "text"
-        eyeOn[1].style.visibility = "hidden"
-        eyeOn[1].style.display = "none"
-        eyeOff[1].style.visibility = "visible"
-        eyeOff[1].style.display = "block"
-        eyeOn[0].style.visibility = "hidden"
-        eyeOn[0].style.display = "none"
-        eyeOff[0].style.visibility = "visible"
-        eyeOff[0].style.display = "block"
-    } else{
+        for (let i = 0; i < eyeOn.length; i++){
+            eyeOn[i].style.visibility = "hidden"
+            eyeOn[i].style.display = "none"
+            eyeOff[i].style.visibility = "visible"
+            eyeOff[i].style.display = "block"
+        }
+    } else {
         pass.type = "password"
-        eyeOff[1].style.visibility = "hidden"
-        eyeOff[1].style.display = "none"
-        eyeOn[1].style.visibility = "visible"
-        eyeOn[1].style.display = "block"
-        eyeOff[0].style.visibility = "hidden"
-        eyeOff[0].style.display = "none"
-        eyeOn[0].style.visibility = "visible"
-        eyeOn[0].style.display = "block"
+        for(let i = 0; i < eyeOn.length; i++){
+            eyeOff[i].style.visibility = "hidden"
+            eyeOff[i].style.display = "none"
+            eyeOn[i].style.visibility = "visible"
+            eyeOn[i].style.display = "block"
+        }
     }
 }
