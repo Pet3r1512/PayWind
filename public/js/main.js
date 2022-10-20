f803345965f8409aada07c4166f4650b1e274b5c
lekhoi2709 <ledinhkhoimd@gmail.com>
2022-10-20 10:37:28 +0700

    routing fixed

--- public/js/main.js
+++ public/js/main.js
@@ -2,24 +2,25 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+app.locals.basedir = path.join(__dirname, '../')
 app.set('views', path.join(__dirname, '../../views'));
 app.set('view engine', 'pug');
 app.use(express.static('public'));
 
 
 app.get('/', (req, res) => {
-	res.render('index.pug');
+	res.render('index');
 });
 
 app.use('/account', require('../../routes/account.route.js'))
 
-app.use((req, res) => {
-	res.status(404).render('../views/errors/error_page', { error_title: '404 - Not Found', error_code: '404', error_header: "Oops! You weren't suppose to see this", error_des: "The page you're looking for no longer exists." });
-});
+// app.use((req, res) => {
+// 	res.status(404).render('../views/errors/error_page', { error_title: '404 - Not Found', error_code: '404', error_header: "Oops! You weren't suppose to see this", error_des: "The page you're looking for no longer exists." });
+// });
 
-app.use((err, req, res, next) => {
-	res.status(500).render('../views/errors/error_page', { error_title: '500 - Server Error', error_code: '500', error_header: "Uh oh! I think i broke it. Please report me to the system administrator!", error_des: "Internal Server Error" });
-})
+// app.use((err, req, res, next) => {
+// 	res.status(500).render('../views/errors/error_page', { error_title: '500 - Server Error', error_code: '500', error_header: "Uh oh! I think i broke it. Please report me to the system administrator!", error_des: "Internal Server Error" });
+// })
 
 const server = app.listen(process.env.PORT || 3000, () => {
 	console.log(`The application started on port ${process.env.PORT || 3000}`);
--- routes/account.route.js
+++ routes/account.route.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 
 router.get('/signin', (req, res) => {
-    return res.render('/views/account/entry/sign_in.pug')
+    return res.render('account/entry/sign_in')
 })
 
 router.get('/signup', (req, res) => {
-    return res.render('account/entry/sign_up.pug')
+    return res.render('account/entry/sign_up')
 })
 
 module.exports = router
