70ad1d1caa7f97cab8588fe72e8b279f1b38e245
lekhoi2709 <ledinhkhoimd@gmail.com>
2022-10-20 11:43:19 +0700

    adjust sign in / up show pass

--- views/assets/javascripts/show_pass.js
+++ views/assets/javascripts/show_pass.js
@@ -1,20 +1,36 @@
-function show_pass(){
-    var pass = document.getElementById('passInput')
-    var eyeOn = document.getElementById('eye-on')
-    var eyeOff = document.getElementById('eye-off')
+function showPass(){
+    var pass = document.getElementById('pass-input')
+    var passConfirm = document.getElementById('pass-confirm')
+    var eyeOn = document.getElementsByClassName('eye-on')
+    var eyeOff = document.getElementsByClassName('eye-off')
+
+    if(passConfirm){
+        if(passConfirm.type == "password"){
+            passConfirm.type = "text"
+        } else{
+            passConfirm.type = "password"
+        }
+    }
 
     if(pass.type == "password"){
         pass.type = "text"
-        eyeOn.style.visibility = "hidden"
-        eyeOn.style.display = "none"
-        eyeOff.style.visibility = "visible"
-        eyeOff.style.display = "block"
+        eyeOn[1].style.visibility = "hidden"
+        eyeOn[1].style.display = "none"
+        eyeOff[1].style.visibility = "visible"
+        eyeOff[1].style.display = "block"
+        eyeOn[0].style.visibility = "hidden"
+        eyeOn[0].style.display = "none"
+        eyeOff[0].style.visibility = "visible"
+        eyeOff[0].style.display = "block"
     } else{
         pass.type = "password"
-        eyeOff.style.visibility = "hidden"
-        eyeOff.style.display = "none"
-        eyeOn.style.visibility = "visible"
-        eyeOn.style.display = "block"
+        eyeOff[1].style.visibility = "hidden"
+        eyeOff[1].style.display = "none"
+        eyeOn[1].style.visibility = "visible"
+        eyeOn[1].style.display = "block"
+        eyeOff[0].style.visibility = "hidden"
+        eyeOff[0].style.display = "none"
+        eyeOn[0].style.visibility = "visible"
+        eyeOn[0].style.display = "block"
     }
-
 }
