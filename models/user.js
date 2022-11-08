ea11b72f7b4433eecfcd44a1584f02d989d41556
lekhoi2709 <ledinhkhoimd@gmail.com>
2022-11-08 23:12:05 +0700

    create user model

---
+++ models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+var userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        min: [6, 'Password must be at least 6 digits!'],
+        validate: {
+            validator: function(v){
+                return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/.test(v)
+            },
+            message: "Password must contains at least 6 characters, 1 uppercase letter, 1 lowercase letter and 1 number"
+        }
+    }
+})
+
+var User = mongoose.model("User", userSchema)
+
+module.exports = User
