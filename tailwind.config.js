424e544be6826e30ad7aa0017f79c19cc25a7afa
phongptt <pttp1512@gmail.com>
2022-11-11 15:57:02 +0700

    Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./views/**/*.pug'],
+	content: ['./views/**/*.pug', './views/*.pug', './views/assets/**/*.pug'],
 	theme: {
 		extend: {
 			fontFamily: {
