0aa20ea0c88dafa4b35baeca8036dc61c3ca88d4
lekhoi2709 <ledinhkhoimd@gmail.com>
2022-11-12 16:52:18 +0700

    rename isExisted() -> emailValidation adjust submit button design

--- views/assets/javascripts/isExisted.js
+++ views/assets/javascripts/isExisted.js
@@ -1,16 +1,28 @@
-async function isExisted() {
+async function emailValidation() {
     const email = document.getElementById("email")
     const emailError = document.getElementById("emailError")
     const submit = document.getElementById("submit")
 
-    const res = await (await fetch(`http://localhost:3000/account/email/isExisted/${email.value}`)).json()
+    if(email.value == ""){
+        email.style.borderColor = "rgb(148, 163, 184)"
+        emailError.style.display = "none"
+        submit.disabled = false
+        submit.style.backgroundColor = "rgb(99, 102, 241)"
+    } else {
+        const res = await (await fetch(`http://localhost:3000/account/email/isExisted/${email.value}`)).json()
 
-    if(res.isExisted == false){
+    if(res.isExisted == false && (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value))){
         emailError.innerHTML = "This email address is available"
         emailError.style.fontSize = "13px"
         email.style.borderColor = "green"
         emailError.style.color = "green"
         emailError.style.display = "block"
+        submit.disabled = false
+        submit.style.backgroundColor = "rgb(99, 102, 241)"
+    } else {
+        emailError.style.display = "none"
+        submit.disabled = true
+        submit.style.backgroundColor = "gray"
     }
 
     if(res.isExisted == true){
@@ -21,5 +33,6 @@ async function isExisted() {
         emailError.style.color = "red"
         submit.disabled = true
         submit.style.backgroundColor = "gray"
+    }
     }
 }
--- views/assets/javascripts/onLoad.js
+++
@@ -1,8 +0,0 @@
-function onLoad(){
-    const emailError = document.getElementById("emailError")
-    const submit = document.getElementById("submit")
-
-    emailError.style.display = "none"
-    submit.disabled = false
-    submit.style.backgroundColor = "rgb(67, 56, 202)"
-}
