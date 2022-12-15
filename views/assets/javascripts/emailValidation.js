fde91fb4a37e78a67217f5a620c85f72b1c7162e
phongptt <pttp1512@gmail.com>
2022-12-15 13:34:47 +0700

    rename

--- views/assets/javascripts/emailValidation.js
+++ views/assets/javascripts/emailValidation.js
@@ -9,10 +9,10 @@ async function emailValidation() {
         submit.disabled = false
         submit.style.backgroundColor = "rgb(99, 102, 241)"
     } else {
-        // const dtbLink = `http://paywind.up.railway.app/account/email/isExisted/${email.value}`
-        const dtbLink = `http://localhost:3000/account/email/isExisted/${email.value}`
+        // const apiLink = `http://paywind.up.railway.app/account/email/isExisted/${email.value}`
+        const apiLink = `http://localhost:3000/account/email/isExisted/${email.value}`
 
-        const res = await (await fetch(dtbLink)).json()
+        const res = await (await fetch(apiLink)).json()
 
         if(res.isExisted == false && (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value))){
             emailError.innerHTML = "This email address is available"
