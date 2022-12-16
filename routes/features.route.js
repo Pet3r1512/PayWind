4eb5cfa78708445b23b699264e961f007a17c8b3
phongptt <pttp1512@gmail.com>
2022-12-16 14:46:30 +0700

    Config routing for Features

--- routes/features.route.js
+++ routes/features.route.js
@@ -1,19 +1,35 @@
 const router = require('express').Router();
 
-router.get('/transfer', (req, res) => {
-    return res.render('../views/user/features/transfer.pug')
+router.get('/', (req, res) => {
+    return res.render('../views/account/user/features/features.pug')
 })
 
-router.get('/deposit', (req, res) => {
-    return res.render('../views/user/features/deposit.pug')
+router.get('/transfer', (req, res) => {
+    return res.render('../views/account/user/features/transfer.pug')
+})
+
+router.get('/recharge', (req, res) => {
+    return res.render('../views/account/user/features/recharge.pug')
 })
 
 router.get('/withdraw', (req, res) => {
-    return res.render('../views/user/features/withdraw.pug')
+    return res.render('../views/account/user/features/withdraw.pug')
 })
 
 router.get('/buy_mobile_card', (req, res) => {
-    return res.render('../views/user/features/buy_mobile_card.pug')
+    return res.render('../views/account/user/features/buy_mobile_card.pug')
+})
+
+router.get('/history', (req, res) => {
+    return res.render('../views/account/user/features/history.pug')
+})
+
+router.get('/add-info', (req, res) => {
+    return res.render('../views/account/user/features/add-info.pug')
+})
+
+router.get('/change-password', (req, res) => {
+    return res.render('../views/account/user/features/change-password.pug')
 })
 
 module.exports = router
