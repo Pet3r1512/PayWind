494217cfedef5549543afce59b08fca4896273cd
Pet3r1512 <89619124+Pet3r1512@users.noreply.github.com>
2022-12-17 19:33:20 +0700

    build validation

---
+++ views/assets/javascripts/rechargeValidation.js
@@ -0,0 +1,6 @@
+function rechargeValidate() {
+    const cardNumber = document.getElementById("cardNumber")
+    const confirmBtn = document.getElementById('confirmBtn')
+
+    console.log(cardNumber)
+}
