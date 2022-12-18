29d6278316ba45ebb5d5f6b5821318dd184c3496
Pet3r1512 <89619124+Pet3r1512@users.noreply.github.com>
2022-12-18 11:38:05 +0700

    remove Admin.js

--- models/admin.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-
-const adminSchema = mongoose.Schema({
-    adminName: String,
-    adminPassword: String,
-    lastLog: Date
-})
-
-module.export = mongoose.model("Admin", adminSchema)
